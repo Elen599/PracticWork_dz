24ba3679208086b49611030b1631d821d261fb5d
«Elena <helenvit599@gmail.com>
2022-09-23 12:16:36 +0500

    Added problem solution 1

---
+++ 1 День недели.py
@@ -0,0 +1,15 @@
+day=int (input("Введите цифру от 1 до 7: "))
+if day == 1:
+    print("Понедельник")
+elif day == 2:
+    print("Вторник")
+elif day == 3:
+    print("Среда")
+elif day == 4:
+    print("Четверг")
+elif day == 5:
+    print("Пятница")
+elif day == 6:
+    print("Суббота - выходной день")
+elif day == 7:
+    print("Воскресенье - выходной день")
